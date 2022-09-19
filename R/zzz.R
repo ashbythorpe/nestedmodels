8f42ea68863830b4caa3cfed27c3b1a938c748a4
Ashby Thorpe <ashbythorpe@gmail.com>
2022-09-19 14:57:51 +0100

    testing

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   make_nested_model()
   
-  safe_multi_predict <- purrr::possibly(multi_predict, otherwise = NULL, 
+  safe_multi_predict <<- purrr::possibly(multi_predict, otherwise = NULL, 
                                         quiet = F)
-  safe_predict <- purrr::possibly(predict, otherwise = NULL, quiet = F)
+  safe_predict <<- purrr::possibly(predict, otherwise = NULL, quiet = F)
 }
